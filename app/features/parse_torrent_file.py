f0abeada0bd65c17925c05188bbb4606dff03062
Sergio Perea <sperea@protonmail.com>
2023-04-04 11:39:24 +0200

    README bit torrent protocol link

--- app/features/parse_torrent_file.py
+++ app/features/parse_torrent_file.py
@@ -21,6 +21,31 @@ Website: https://sperea.es
 Please give credit to the author and the website when using or redistributing this code.
 """
 
+"""
+bencoding
+---------
+
+Strings are length-prefixed base ten followed by a colon and the string. For example 4:spam corresponds to 'spam'.
+
+Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'. 
+For example i3e corresponds to 3 and i-3e corresponds to -3. 
+
+Integers have no size limitation. i-0e is invalid. 
+
+All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to 0.
+
+Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'. 
+For example l4:spam4:eggse corresponds to ['spam', 'eggs'].
+
+Dictionaries are encoded as a 'd' followed by a list of alternating keys and their corresponding values followed by an 'e'. 
+For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. 
+
+Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics).
+
+For more information on the BitTorrent protocol, 
+please refer to the [official BitTorrent specification](https://www.bittorrent.org/beps/bep_0003.html).
+"""
+
 import urllib.parse
 from app.repositories.meta_info import TorrentInfo, MetaInfo, TorrentFile, Tracker, FileInfo
 
