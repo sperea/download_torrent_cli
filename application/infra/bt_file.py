3e666298e44ebba7036b0e5f432b86c0275c43b1
Sergio Perea <sperea@protonmail.com>
2023-04-04 10:32:02 +0200

    more test for Meta Info

--- application/infra/bt_file.py
+++ application/infra/bt_file.py
@@ -1,5 +1,25 @@
 
+"""
+Copyright (C) 2023 Sergio Perea
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Author: Sergio Perea
+Website: https://sperea.es
+
+Please give credit to the author and the website when using or redistributing this code.
+"""
 
 """
 This code defines two classes: BTFile and BTFileCollection. The purpose of this code is to manage the reading and writing of files in a torrent file collection, as well as to compute SHA-1 hashes for data verification. This is particularly useful in applications that work with torrent files, such as torrent download clients.
